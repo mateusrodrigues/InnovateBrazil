a7823bcffbc5bb698eb8f98f490388f92c145c67
Mateus Rodrigues de Morais <mateus.rodrigues@live.com>
2016-11-28 13:26:08 -0300

    New homepage template

--- src/InnovateBrazil.Web/wwwroot/_references.js
+++ src/InnovateBrazil.Web/wwwroot/_references.js
@@ -1,4 +1,7 @@
 ﻿/// <autosync enabled="true" />
+/// <reference path="js/creative.min.js" />
+/// <reference path="js/jquery.magnific-popup.min.js" />
+/// <reference path="js/scrollreveal.min.js" />
 /// <reference path="js/site.js" />
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
 /// <reference path="lib/jquery/dist/jquery.js" />
